914a3c0c640f049fff8222627ee0888a69b9dd71
Frederik Shull <frederik.shull@web.de>
2018-10-21 00:50:46 +0200

    navbar fix

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 						<li class="<?php if ($active == "miscellaneous") {echo("active");}?> waves-effect"><a href="./?page=miscellaneous">Miscellaneous</a></li>
 					</ul>
 					<ul class="right hide-on-med-and-down">
-						<li><a class="<?php if ($active == "impressum") {echo("active");}?> waves-effect" href="./?page=impressum">Impressum</a></li>
+						<li class="<?php if ($active == "impressum") {echo("active");}?> waves-effect"><a href="./?page=impressum">Impressum</a></li>
 					</ul>
 					<a href="#" data-target="slide-out" class="sidenav-trigger hide-on-large-only"><i class="material-icons">menu</i></a>
 					<span class="brand-logo center hide-on-large-only">Home</span>
