cf6e0d04ec540dad2a492023d9aa429d48f3474f
Frederik Shull <frederik.shull@web.de>
2018-10-22 20:18:50 +0200

    switch of titles

--- pages/hs-fulda-ai1001_lerntagebuch.php
+++ pages/hs-fulda-ai1001_lerntagebuch.php
@@ -1,8 +1,8 @@
 <div class="container">
-	<h3 class="center">Journal</h3>
+	<h3 class="center">Lerntagebuch - AI1001</h3>
 	<div class="card">
 		<div class="card-content">
-		<h4>Lerntagebuch - AI1001</h4>
+		<h4>Journal</h4>
 			<?php echo(file_get_contents('./resources/hs-fulda/ai1001-lerntagebuch.html'))?>
 		</div>
 	</div>
