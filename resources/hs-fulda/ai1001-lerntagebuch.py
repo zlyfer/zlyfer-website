83e6183f22f84c9e7817ed4d5863175cdbbf714c
Frederik Shull <frederik.shull@web.de>
2018-10-23 17:40:01 +0200

    update

--- resources/hs-fulda/ai1001-lerntagebuch.py
+++ resources/hs-fulda/ai1001-lerntagebuch.py
@@ -5,36 +5,25 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 
-# os.chdir("home/zlyfer/zlyfer-website/resources/")
+os.chdir("home/zlyfer/zlyfer-website/resources/hs-fulda/")
 logging.basicConfig(format="\n%(levelname)s: @'%(asctime)s' in '%(name)s':\n> %(message)s", level=logging.INFO)
 
-credentials = json.loads(codecs.open('./credentials.json', 'r', 'utf-8').read())
+credentials = json.loads(codecs.open('/home/zlyfer/tokens/credentials.json', 'r', 'utf-8').read())
 url1 = 'https://elearning.hs-fulda.de/ai/login/index.php'
 url2 = 'https://elearning.hs-fulda.de/ai/mod/journal/edit.php?id=20748'
 
-def getHTML():
-    session = requests.session()
-    session.post(url1, data=credentials)
-    response = session.get(url2).text
-    file = codecs.open('.junk', 'w', 'utf-8')
-    soup = BeautifulSoup(response, "html.parser")
-    file.write(soup.get_text("\n"))
-    file.close
-
-def getJournal():
-    file = codecs.open('.junk', 'r', 'utf-8')
-    content = file.readlines()
-    file.close
-    record = False
-    journal = ''
-    for line in content:
-        if 'crawler_end' in line:
-            record = False
-        if record == True:
-            journal += line
-        if 'crawler_start' in line:
-            record = True
-    codecs.open('ai1001-lerntagebuch.html', 'w', 'utf-8').write(journal)
-
-getHTML()
-getJournal()
+session = requests.session()
+session.post(url1, data=credentials)
+response = session.get(url2).text
+soup = BeautifulSoup(response, "html.parser")
+content = soup.get_text("\n").split("\n")
+record = False
+journal = ''
+for line in content:
+    if 'crawler_end' in line:
+        record = False
+    if record == True:
+        journal += line + "\n"
+    if 'crawler_start' in line:
+        record = True
+codecs.open('ai1001-lerntagebuch.html', 'w', 'utf-8').write(journal)
