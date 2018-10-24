c3d5498e54ff8b7d76360ea2b353f08d342a1120
Frederik Shull <frederik.shull@web.de>
2018-10-24 20:18:05 +0200

    pause

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,3 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+	M.Parallax.init(document.querySelectorAll('.parallax'), {});
+	M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
+	if (getCookie("cookie") == "") {
+		M.toast({
+			html: `<span>We use cookies!</span>
+		<button class="waves-effect btn-flat toast-action grey-text text-lighten-5"><a href="./?page=impressum#cookies"><i class="material-icons grey-text text-lighten-5">info_outline</i></a></button>`,
+			classes: 'green darken-1',
+			displayLength: 10000,
+			completeCallback: () => {
+				setCookie("cookie", "true", "365");
+			}
+		})
+		setCookie("cookie", "true", "365");
+	}
+	// setai1001lerntagebuchtime();
+});
+
 // https://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
 	var d = new Date();
@@ -5,6 +23,7 @@ function setCookie(cname, cvalue, exdays) {
 	var expires = "expires=" + d.toUTCString();
 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
 // https: //www.w3schools.com/js/js_cookies.asp
 function getCookie(cname) {
 	var name = cname + "=";
@@ -22,19 +41,74 @@ function getCookie(cname) {
 	return "";
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-	M.Parallax.init(document.querySelectorAll('.parallax'), {});
-	M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
-	if (getCookie("cookie") == "") {
-		M.toast({
-			html: `<span>We use cookies!</span>
-		<button class="waves-effect btn-flat toast-action grey-text text-lighten-5"><a href="./?page=impressum#cookies"><i class="material-icons grey-text text-lighten-5">info_outline</i></a></button>`,
-			classes: 'green darken-1',
-			displayLength: 10000,
-			completeCallback: () => {
-				setCookie("cookie", "true", "365");
+function setai1001lerntagebuchtime() {
+	let span = document.getElementById('stringtime');
+	if (span) {
+
+		let string = span.innerHTML;
+		let shours = parseInt(string.slice(0, 2));
+		let sminutes = parseInt(string.slice(3, 5));
+		let sseconds = parseInt(string.slice(6, 8));
+
+		{
+			let current = new Date();
+			let chours = parseInt(current.getHours());
+			let cminutes = parseInt(current.getMinutes());
+			let cseconds = parseInt(current.getSeconds());
+
+			let dhours = chours + shours;
+			let dminutes = cminutes + sminutes;
+			let dseconds = cseconds + sseconds;
+
+			let fix = fixtime(dhours, dminutes, dseconds);
+			let fhours = fix[0];
+			let fminutes = fix[1];
+			let fseconds = fix[2];
+
+			dhours = fhours - chours;
+			dminutes = fminutes - cminutes;
+			dseconds = fseconds - cseconds;
+
+			fix = fixtime(dhours, dminutes, dseconds);
+			fhours = fix[0];
+			fminutes = fix[1];
+			fseconds = fix[2];
+
+			// span.innerHTML = `${fhours}:${fminutes}:${fseconds}`;
+		}
+
+		function fixtime(h, m, s) {
+
+			// if (s >= 60) {
+			m += Math.floor(s / 60);
+			s = Math.abs(s % 60);
+			// }
+			//  else if (s < 0) {
+			// 	s = Math.abs(s);
+			// 	m -= Math.floor(s / 60);
+			// 	s = s % 60;
+			// }
+			// if (m >= 60) {
+			h += Math.floor(m / 60);
+			m = Math.abs(m % 60);
+			// }
+			if (h > 24) {
+				h -= 24;
+			} else if (h < 0) {
+				h += 24;
 			}
-		})
-		setCookie("cookie", "true", "365");
+
+			if (h < 10) {
+				h = `0${h}`;
+			}
+			if (m < 10) {
+				m = `0${m}`;
+			}
+			if (s < 10) {
+				s = `0${s}`;
+			}
+
+			return ([h, m, s]);
+		}
 	}
-});
+}
--- pages/hs-fulda-ai1001_lerntagebuch.php
+++ pages/hs-fulda-ai1001_lerntagebuch.php
@@ -1,23 +1,16 @@
 <?php
-$minutes = 0;
-$seconds = 0;
 clearstatcache();
 $currenttime = time();
 $lasttime = filemtime('resources/hs-fulda/ai1001-lerntagebuch.html');
-$years = date("Y", $lasttime);
-$months = date("m", $lasttime);
-$days = date("d", $lasttime);
-$hours = date("h", $lasttime);
-$minutes = date("i", $lasttime);
-$seconds = date("s", $lasttime);
-$nexttime = strtotime($years."-".$months."-".$days." ".$hours.":".$minutes.":".$seconds." +1 hour");
+$nexttime = strtotime(date("Y-m-d H:i:s", $lasttime)." +1 hour");
+echo ($lasttime."--".$nexttime."--".$currenttime);
 $deltatime = $nexttime - $currenttime;
 ?>
 <div class="container">
 	<h3 class="center">Lerntagebuch - AI1001</h3>
 	<div class="card">
 		<div class="card-content">
-			<strong class="right">Nächste Aktualisierung: <?php echo(date("h:i:s", $deltatime));?></strong>
+			<!-- <strong class="right">Nächste Aktualisierung: <?php echo(date("H:i:s", $deltatime));?></strong> -->
 		<h4>Journal</h4>
 			<?php echo(file_get_contents('./resources/hs-fulda/ai1001-lerntagebuch.html'))?>
 		</div>
