5da76089bb4220ad0b978d096124e001015d4ca6
Frederik Shull <frederik.shull@web.de>
2018-10-24 17:48:46 +0200

    test

--- pages/hs-fulda-ai1001_lerntagebuch.php
+++ pages/hs-fulda-ai1001_lerntagebuch.php
@@ -2,25 +2,22 @@
 $minutes = 0;
 $seconds = 0;
 clearstatcache();
-$lasttime = filemtime('resources/hs-fulda/ai1001-lerntagebuch.html');
 $currenttime = time();
+$lasttime = filemtime('resources/hs-fulda/ai1001-lerntagebuch.html');
 $years = date("Y", $lasttime);
 $months = date("m", $lasttime);
 $days = date("d", $lasttime);
 $hours = date("h", $lasttime);
 $minutes = date("i", $lasttime);
 $seconds = date("s", $lasttime);
-// $nexttime = strtotime($years."-".$months."-".$days." ".$hours.":30:00");
 $nexttime = strtotime($years."-".$months."-".$days." ".$hours.":".$minutes.":".$seconds." +1 hour");
 $deltatime = $nexttime - $currenttime;
-$minutes = date("i", $deltatime);
-$seconds = date("s", $deltatime);
 ?>
 <div class="container">
 	<h3 class="center">Lerntagebuch - AI1001</h3>
 	<div class="card">
 		<div class="card-content">
-			<strong class="right">Nächste Aktualisierung: <?php echo($minutes.":".$seconds);?></strong>
+			<strong class="right">Nächste Aktualisierung: <?php echo(date("h:i:s", $deltatime));?></strong>
 		<h4>Journal</h4>
 			<?php echo(file_get_contents('./resources/hs-fulda/ai1001-lerntagebuch.html'))?>
 		</div>
