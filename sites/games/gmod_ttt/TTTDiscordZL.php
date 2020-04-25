82ac833a7e111d492fcf01387a18a9100948a364
Frederik Shull <frederik.shull@protonmail.com>
2020-04-25 15:59:28 +0200

    update

--- sites/games/gmod_ttt/TTTDiscordZL.php
+++ sites/games/gmod_ttt/TTTDiscordZL.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-include('/home/zlyfer/tokens/sql_credentials.php');
+include '/home/zlyfer/tokens/sql_credentials.php';
 $sqlServer = "127.0.0.1";
 $sqlDatabase = "TTTDiscordZL";
 
@@ -8,43 +8,51 @@ $sql = new MySQLi($sqlServer, $sqlUser, $sqlPassword, $sqlDatabase);
 $sql->set_charset("utf8");
 
 if (isset($_GET['token'])) {
-	$Token = $_GET['token'];
-	if (isset($_GET['guildid'])) {
-		$GuildID = $_GET['guildid'];
-		$result = $sql->query("SELECT `Token` FROM `tokens` WHERE `GuildID` = '$GuildID'")->fetch_all();
-		if ($result) {
-			if ($Token == $result[0][0]) {
-				echo nl2br ("Access granted!\n");
-				if (isset($_GET['mute'])) {
-					$mute = $_GET['mute'];
-					if ($mute == "all") {
-						$sql->query("UPDATE `$GuildID` SET `Muted` = '1' WHERE 1");
-						echo("Muted everyone!");
-					} else {
-						$sql->query("UPDATE `$GuildID` SET `Muted` = '1' WHERE `SteamID64` = '$mute'");
-						echo("Muted $mute!");
-					}
-				}
-				if (isset($_GET['unmute'])) {
-					$unmute = $_GET['unmute'];
-					if ($unmute == "all") {
-						$sql->query("UPDATE `$GuildID` SET `Muted` = '0' WHERE 1");
-						echo("Unmuted everyone!");
-					} else {
-						$sql->query("UPDATE `$GuildID` SET `Muted` = '0' WHERE `SteamID64` = '$unmute'");
-						echo("Unmuted $unmute!");
-					}
-				}
-			} else {
-				echo nl2br ("Access denied!\n");
-			}
-		} else {
-			echo nl2br ("Database error!\n");
-		}
-	} else {
-		echo nl2br ("No GuildID specified!\n");
-	}
+  $Token = $_GET['token'];
+  if (isset($_GET['guildid'])) {
+    $GuildID = $_GET['guildid'];
+    $result = $sql->query("SELECT `Token` FROM `tokens` WHERE `GuildID` = '$GuildID'")->fetch_all();
+    if ($result) {
+      if ($Token == $result[0][0]) {
+        echo nl2br("Access granted!\n");
+        if (isset($_GET['mute'])) {
+          $mute = $_GET['mute'];
+          if ($mute == "all") {
+            $sql->query("UPDATE `$GuildID` SET `Muted` = '1' WHERE 1");
+            echo "Muted everyone!";
+          } else {
+            $sql->query("UPDATE `$GuildID` SET `Muted` = '1' WHERE `SteamID64` = '$mute'");
+            echo "Muted $mute!";
+          }
+        }
+        if (isset($_GET['unmute'])) {
+          $unmute = $_GET['unmute'];
+          if ($unmute == "all") {
+            $sql->query("UPDATE `$GuildID` SET `Muted` = '0' WHERE 1");
+            echo "Unmuted everyone!";
+          } else {
+            $sql->query("UPDATE `$GuildID` SET `Muted` = '0' WHERE `SteamID64` = '$unmute'");
+            echo "Unmuted $unmute!";
+          }
+        }
+        if (isset($_GET['linkDiscordID']) && isset($_GET['linkSteamID64'])) {
+          $discordId = $_GET['linkDiscordID'];
+          $steamId64 = $_GET['linkSteamID64'];
+          $sql->query(
+            "INSERT INTO `$GuildID` (`DiscordID`, `SteamID64`, `Muted`) VALUES ('$discordId', '$steamId64', '0') ON DUPLICATE KEY UPDATE DiscordID = $discordId"
+          );
+          echo "Linked SteamID64 $steamId64 with DiscordID $discordId on Discord Server $GuildID!";
+        }
+      } else {
+        echo nl2br("Access denied!\n");
+      }
+    } else {
+      echo nl2br("Database error!\n");
+    }
+  } else {
+    echo nl2br("No GuildID specified!\n");
+  }
 } else {
-	echo nl2br ("No token specified!\n");
+  echo nl2br("No token specified!\n");
 }
 ?>
